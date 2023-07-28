e811b655cec6bafa6946801da61f830d73b5c0ef
kevin-foreman <kevin.o.foreman2@gmail.com>
2023-07-28 17:49:30 -0600

    Added routes for main page and cart, added navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,31 @@
 import { useState } from 'react'
 import missfitImage from './assets/missfit-logo-1.png';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css'
+import Navbar from './components/Navbar.jsx';
+import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className='App'>
+      <div>
+        <Router>
+          <Navbar />
           <a>
-          <img src={missfitImage} className='logo'></img>
+            <img src={missfitImage} className='logo'></img>
           </a>
-          <Router>
-            {/* <Navbar></Navbar> */}
-            <Routes>
-              <Route path="/" />
-              <Route path="/cart" />
-            </Routes>
-          </Router>
-      </div>
-      <h1>Swaggy Pop Culture Merch</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Order count is {count}
-        </button>
+          <Routes>
+            <Route path="/" />
+            <Route path="/cart" />
+          </Routes>
+        </Router>
+        <h1>Swaggy Pop Culture Merch</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            Order count is {count}
+          </button>
+        </div>
       </div>
     </>
   )
