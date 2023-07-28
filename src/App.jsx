0d5014ff8df5be7a9e4a9b5889e22e5e75fdabdf
kevin-foreman <kevin.o.foreman2@gmail.com>
2023-07-28 17:20:28 -0600

    Added React router setup and initial navbar framework

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import missfitImage from './assets/missfit-logo-1.png';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 
 function App() {
@@ -7,10 +8,17 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className='App'>
           <a>
           <img src={missfitImage} className='logo'></img>
           </a>
+          <Router>
+            {/* <Navbar></Navbar> */}
+            <Routes>
+              <Route path="/" />
+              <Route path="/cart" />
+            </Routes>
+          </Router>
       </div>
       <h1>Swaggy Pop Culture Merch</h1>
       <div className="card">
