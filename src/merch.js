f6cffa0a5c1d72c580d672788a26924239a4e7aa
kevin-foreman <kevin.o.foreman2@gmail.com>
2023-07-28 20:27:29 -0600

    Added a new product to the lineup

--- src/merch.js
+++ src/merch.js
@@ -1,11 +1,11 @@
-import gamerCup from './assets/gamer-cup.png';
 import cupTest from './assets/cup-test.png';
 import stickerPack23 from './assets/gamer-stickers.png';
+import missFitHoodie from './assets/gamer-hoodie-v1.png';
 
 export const MERCH = [
     {
         id: 1,
-        productName: "Gamer Cup",
+        productName: "Gamer Tumbler",
         price: 29.99,
         productImage: cupTest,
     },
@@ -14,5 +14,11 @@ export const MERCH = [
         productName: "Stickers",
         price: 4.99,
         productImage: stickerPack23,
+    },
+    {
+        id: 3,
+        productName: "Epic Hoodie",
+        price: 79.99,
+        productImage: missFitHoodie,
     }
 ]
